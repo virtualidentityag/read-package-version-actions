37cd81c4c12fccd69f9574f9071acda5ce27bdfd
Ralf Hafner <ralf.hafner@virtual-identity.com>
2020-04-24 13:18:46 +0200

    fix: 🐛 add propertyKey param to function call

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { getPackageProperty } from './getPackageProperty';
 async function run() {
   try {
     const path = core.getInput('path');
+    const propertyKey = core.getInput('propertyKey');
 
     core.debug(`Load package.json at ${path}`);
 
-    const result = getPackageProperty(path);
+    const result = getPackageProperty(path, propertyKey);
 
     core.debug(`set output: propertyValue: ${result}`);
     core.setOutput('propertyValue', result);
