cbc4a7b9c90015067cbc56759df0cd7e0fcaa222
Ralf Hafner <ralf.hafner@virtual-identity.com>
2020-04-24 13:21:24 +0200

    feat: 🎸 read any property not just version (#1)
    * feat: 🎸 read any property not just version

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import * as core from '@actions/core';
-import { getPackageVersion } from './getPackageVersion';
+import { getPackageProperty } from './getPackageProperty';
 
 async function run() {
   try {
     const path = core.getInput('path');
+    const propertyKey = core.getInput('propertyKey');
 
     core.debug(`Load package.json at ${path}`);
 
-    const result = getPackageVersion(path);
+    const result = getPackageProperty(path, propertyKey);
 
-    core.debug(`set output: version: ${result}`);
-    core.setOutput('version', result);
+    core.debug(`set output: propertyValue: ${result}`);
+    core.setOutput('propertyValue', result);
   } catch (error) {
     core.setFailed(error.message);
   }
